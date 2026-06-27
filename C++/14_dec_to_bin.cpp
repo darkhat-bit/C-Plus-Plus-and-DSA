8f957a1bd2722d17fbe6eeb81a1b116d0ea9041a
darkhat-bit <jaiswalrishabh704@gmail.com>
2026-06-27 22:46:57 +0530

    Add decimal to binary conversion program in C++

---
+++ C++/14_dec_to_bin.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+    cout << "Enter Decimal Number: ";
+    cin >> n;
+
+    int ans = 0;
+    int base = 1;
+
+    while (n != 0) {
+        int bit = n & 1;
+        ans = (bit * base) + ans;
+        n >>= 1;
+        base *= 10;
+        cout << ans << " " << n << " " << base << endl;
+    }
+
+    cout << "Binary Output" << ans << endl;
+    return 0;
+}
