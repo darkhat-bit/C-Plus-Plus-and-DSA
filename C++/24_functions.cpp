1ea16dd5b0ab814c5cb612b835d593ed2d7b85e4
darkhat-bit <jaiswalrishabh704@gmail.com>
2026-06-29 18:09:07 +0530

    Add power function program to calculate exponentiation in C++

---
+++ C++/24_functions.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int power() {
+    int num1, num2;
+    cin >> num1 >> num2;
+
+    int ans = 1;
+    for (int i = 1; i <= num2; i++) {
+        ans *= num1;
+    }
+    return ans;
+}
+
+int main() {
+    int answer = power();
+    cout << "Answer is: " << answer << endl;
+    return 0;
+}
