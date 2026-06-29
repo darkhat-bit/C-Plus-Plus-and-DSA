2831f32e337bfd9fb073da66e2bf18bf035f797a
darkhat-bit <jaiswalrishabh704@gmail.com>
2026-06-29 18:11:35 +0530

    Add even-odd checker program in C++

---
+++ C++/25_even_odd_checker.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+bool isEven(int a) {
+    if (a & 1) {
+        return false;
+    }
+    return true;
+}
+
+int main() {
+    int num;
+    cin >> num;
+    if (isEven(num)) {
+        cout << "Number is Even" << endl;
+    } else {
+        cout << "Number is Odd" << endl;
+    }
+    return 0;
+}
