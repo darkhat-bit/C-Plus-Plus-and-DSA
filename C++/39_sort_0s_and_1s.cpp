3df240bad3c9a226779f0190a0901848cf6fa1a8
darkhat-bit <jaiswalrishabh704@gmail.com>
2026-06-30 17:06:00 +0530

    Add program to sort an array of 0s and 1s in C++

---
+++ C++/39_sort_0s_and_1s.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+using namespace std;
+
+void sortOne(int arr[], int size) {
+    int left = 0;
+    int right = size - 1;
+
+    while (left < right) {
+        while (arr[left] == 0 && left < right) {
+            left++;
+        }
+        while (arr[right] == 1 && left < right) {
+            right--;
+        }
+        if (left < right) {
+            swap(arr[left], arr[right]);
+            left++;
+            right--;
+        }
+    }
+}
+
+void printArray(int arr[], int size) {
+    for (int i = 0; i < size; i++) {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+}
+
+int main() {
+    int arr[8] = {1, 1, 0, 0, 1, 0, 1, 0};
+    sortOne(arr, 8);
+    printArray(arr, 8); // Output: 0 0 0 0 1 1 1 1
+    return 0;
+}
