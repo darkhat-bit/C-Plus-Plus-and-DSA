1e1f9853d3b12186eb69ba36b7e6b8f3e21edba1
darkhat-bit <jaiswalrishabh704@gmail.com>
2026-06-30 16:50:03 +0530

    Add program to find pairs with a given sum in an array

---
+++ C++/38_pair_sum.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+void pairSum(int arr[], int size, int target) {
+    int i = 0;
+    for (i; i < size; i++) {
+        for (int j = i + 1; j < size; j++) {
+            if (arr[i] + arr[j] == target) {
+                cout << arr[i] << " " << arr[j] << endl;
+            }
+        }
+    }
+}
+
+int main() {
+    int arr[5] = {1, 2, 3, 4, 5};
+    pairSum(arr, 5, 5);
+    return 0;
+}
